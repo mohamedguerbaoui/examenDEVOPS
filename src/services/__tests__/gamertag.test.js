9c326586d933b3ab70146fded8c191c49622e377
mohamedguerbaoui <mohamed.guerbaoui@student.vinci.be>
2024-01-15 16:52:20 +0100

    feat:TDD test

--- src/services/__tests__/gamertag.test.js
+++ src/services/__tests__/gamertag.test.js
@@ -7,22 +7,17 @@ describe('Gamer Tag Validation', () => {
     expect(validateGamerTag('abcd')).toBe('Invalid - gamertag length must be at least 8 characters');
   });
 
+
   it('should be invalid if it does not contain a special character', () => {
-    expect(validateGamerTag('abcdefgh')).toBe('Invalid - gamertag must contain at least a special character');
+    expect(validateGamerTag('abcd1234')).toBe('Invalid - gamertag must contain at least a special character');
   });
 
   it('should be invalid if it does not contain a number', () => {
-    expect(validateGamerTag('abcdefgh$')).toBe('Invalid - gamertag must contain at least a number');
+    expect(validateGamerTag('abcd$!è§à_')).toBe('Invalid - gamertag must contain at least a number');
   });
 
   it('should be valid if it meets all requirements', () => {
     expect(validateGamerTag('validTag1$')).toBe('Valid');
   });
 
-  it('should handle other cases', () => {
-    // Add more test cases to cover different branches
-    expect(validateGamerTag('abcd$')).toBe('Invalid - gamertag must contain at least a number');
-    expect(validateGamerTag('abcdefgh1')).toBe('Invalid - gamertag must contain at least a special character');
-    expect(validateGamerTag('abcd1')).toBe('Invalid - gamertag must contain at least a special character');
-  });
 });
