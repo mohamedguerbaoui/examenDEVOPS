6c0eb2d95eeb7af10724f57615de47770a30a106
mohamedguerbaoui <mohamed.guerbaoui@student.vinci.be>
2024-01-15 16:35:02 +0100

    feat:TDD validation

--- src/services/__tests__/validations.test.js
+++
@@ -1,9 +0,0 @@
-// TODO: Create tests suite for validation function
-const { isValid } = require('../validations');
-
-describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid();
-        expect(result).toBeDefined();
-    });
-});
--- src/services/validations.js
+++
@@ -1,4 +0,0 @@
-// TODO: Refactor validation function to follow the defined validations rules
-const isValid = () => Math.random() >= 0.5;
-
-exports.isValid = isValid;
