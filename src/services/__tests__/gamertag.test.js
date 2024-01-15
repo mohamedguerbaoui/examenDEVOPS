a71822deb6687ee3e0880919fe20d95b756846f8
mohamedguerbaoui <mohamed.guerbaoui@student.vinci.be>
2024-01-15 16:37:45 +0100

    feat:TDD special character

--- src/services/__tests__/gamertag.test.js
+++ src/services/__tests__/gamertag.test.js
@@ -7,4 +7,10 @@ describe('Gamer Tag Validation', () => {
     expect(validateGamerTag('abcd')).toBe('Invalid - gamertag length must be at least 8 characters');
   });
 
+
+  it('should be invalid if it does not contain a special character', () => {
+    expect(validateGamerTag('abcd1234')).toBe('Invalid - gamertag must contain at least a special character');
+  });
+
+
 });
--- src/services/gamertag.js
+++ src/services/gamertag.js
@@ -4,6 +4,14 @@ function validateGamerTag(gamerTag) {
     if (gamerTag.length < 8) {
       return 'Invalid - gamertag length must be at least 8 characters';
     }
+
+
+    const specialCharacters = ['&', '$', '!', 'è', '§', 'à', '_'];
+  if (!specialCharacters.some(char => gamerTag.includes(char))) {
+    return 'Invalid - gamertag must contain at least a special character';
+  }
+
+  
   }
   
   module.exports = validateGamerTag;
